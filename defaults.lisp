41e29753c366cce8235a3016b187708b68b10c06
Shinmera <shinmera@tymoon.eu>
2019-12-14 16:35:07 +0100

    Implement default backend selector

--- defaults.lisp
+++ defaults.lisp
@@ -4,6 +4,43 @@
  Author: Nicolas Hafner <shinmera@tymoon.eu>
 |#
 
-(in-package #:org.shirakumo.file-select.gtk)
+(in-package #:org.shirakumo.file-select)
 
-;; TODO: determine best default
+(define-condition file-select-backend-not-found (warning)
+  ()
+  (:report (lambda (c s) (format s "No usable backend for file-select could be found!"))))
+
+(defun split (char string)
+  (let ((paths ())
+        (buffer (make-string-output-stream)))
+    (flet ((maybe-commit ()
+             (let ((string (get-output-stream-string buffer)))
+               (when (string/= string "")
+                 (push (parse-native-namestring string) paths)))))
+      (loop for c across string
+            do (if (char= c char)
+                   (maybe-commit)
+                   (write-char c buffer))
+            finally (maybe-commit)))
+    (nreverse paths)))
+
+(defun find-in-path (file)
+  (dolist (path (split #\: (uiop:getenv "PATH")))
+    (when (probe-file (merge-pathnames file path))
+      (return (merge-pathnames file path)))))
+
+(defun determine-default-backend ()
+  (cond ((find :win32 *features*)
+         'org.shirakumo.file-select.win32:win32)
+        ((find :darwin *features*)
+         'org.shirakumo.file-select.ios:ios)
+        ((find-in-path "zenity")
+         'org.shirakumo.file-select.zenity:zenity)
+        ((ignore-errors (cffi:load-foreign-library 'org.shirakumo.file-select.gtk:gtk))
+         (cffi:close-foreign-library 'org.shirakumo.file-select.gtk:gtk)
+         'org.shirakumo.file-select.gtk:gtk)
+        (T
+         (warn 'file-select-backend-not-found))))
+
+(unless (boundp '*default-backend*)
+  (setf *default-backend* (determine-default-backend)))
